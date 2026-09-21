7406ad1d9ab023cf7cbb36cdac3743e927eba5e8
Paola Franco Gregorio <250859@utxicotepec.edu.mx>
2026-09-21 09:31:20 -0600

    le agregue notas a la practica

--- Practicas-U1/practica2_burbuja.py
+++ Practicas-U1/practica2_burbuja.py
@@ -2,9 +2,9 @@ lista = [5,2,6,3,4,8,9,10,7,1,8,6,4,9,10]
 n = len(lista) 
 swapped = True
 while swapped:
-    swapped = False
+    swapped = False       #variable booleana=swapped
     for i in range(n-1):   
-        if lista[i]>lista[i+1]:  
+        if lista[i]>lista[i+1]:  #se cambia el signo para ascendenete y descentdente <>
             lista[i],lista[i+1]=lista[i+1],lista[i]  
             swapped = True
-print("Orden Ascendente:",lista)            
+print("Orden Ascendente:",lista)         #indice = 0-14   
